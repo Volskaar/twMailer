ee41dc86a4ae23396a491f9b4ea8a323ffb83317
eierschaedl <if21b172@technikum-wien.at>
2023-10-23 00:11:40 +0200

    client input funktioniert und wird korrekt an server geschickt

--- twMailer/myclient.cpp
+++ twMailer/myclient.cpp
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <vector>
+#include <iostream>
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -14,13 +16,95 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
-int main(int argc, char **argv)
-{
+std::string receiveInput(){
+    std::string input;
+    std::cout << ">> ";
+    std::getline(std::cin, input);
+    return input;
+}
+
+std::string receiveUser(std::string role){
+    std::string input;
+    bool wrongInput = true;
+
+    while(wrongInput) {
+        std::cout << "Please enter " << role << "username (max. 8 chars, a - z, 0 - 9)" << std::endl;
+        std::cout << ">> ";
+        std::getline(std::cin, input);
+
+        if (input.size() > 8) {
+            input.erase();
+            std::cout << "Input too long. ";
+        } else {
+            for (unsigned int i = 0; i < input.length(); i++) {
+                if (!(std::islower(input[i]) || std::isdigit(input[i]))) {
+                    input.erase();
+                    std::cout << "Wrong characters. ";
+                    break;
+                }
+            }
+        }
+        if (input[0]) {
+            wrongInput = false;
+        }
+    }
+    return input;
+}
+
+std::string receiveSubject(){
+    std::string inputSubject;
+    bool exitCondition = false;
+
+    std::cout << "Enter your message's subject: " << std::endl;
+    while(!exitCondition){
+        std::cout << ">> ";
+        std::getline(std::cin, inputSubject);
+        if(inputSubject.length() <= 80){
+            exitCondition = true;
+        }
+        else{
+            std::cout << "Subject too long (max. 80 characters). ";
+        }
+    }
+    return inputSubject;
+}
+
+std::string receiveMessage(){
+    std::vector<std::string> inputs;
+    std::string input;
+    bool exitCondition = false;
+    int messageNo = 0;
+
+    std::cout << "Enter your message: " << std::endl;
+    while(!exitCondition){
+        std::cout << ">> ";
+        std::getline(std::cin, input);
+        if(input == "." && messageNo != 0){
+            exitCondition = true;
+        }
+        inputs.push_back(input);
+        messageNo++;
+        input.erase();
+    }
+
+    for(auto & iter : inputs){
+        input += iter;
+        input += '\n';
+    }
+    input[input.size() - 1] = '\0';
+    return input;
+}
+
+std::string receiveNumber(){
+    return "123";
+}
+
+int main(int argc, char **argv){
    int create_socket;
    char buffer[BUF];
    struct sockaddr_in address;
    int size;
-   int isQuit;
+   int isQuit = 0;
 
    ////////////////////////////////////////////////////////////////////////////
    // CREATE A SOCKET
@@ -85,43 +169,136 @@ int main(int argc, char **argv)
       printf("%s", buffer); // ignore error
    }
 
-   do
-   {
-      printf(">> ");
-      if (fgets(buffer, BUF, stdin) != NULL)
-      {
-         int size = strlen(buffer);
-         // remove new-line signs from string at the end
-         /*
-         if (buffer[size - 2] == '\r' && buffer[size - 1] == '\n')
-         {
-            size -= 2;
-            buffer[size] = 0;
-         }
-         else if (buffer[size - 1] == '\n')
-         {
-            --size;
-            buffer[size] = 0;
-         }
-          */
-         isQuit = strcmp(buffer, "quit") == 0;
+    int inputCorrect = 0;
+    std::string input;
+    std::vector<std::string> inputs;
+
+   do{
+       while(inputCorrect == 0){
+           input = receiveInput();
+
+           if(input == "SEND"){
+               std::cout << "SEND command " << std::endl;
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveUser("Sender's ");
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveUser("Receiver's ");
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveSubject();
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveMessage();
+               inputs.push_back(input);
+               input.erase();
+
+               for(auto & iter : inputs){
+                   input += iter;
+                   input += '\n';
+               }
+
+               strcpy(buffer, input.c_str());
+               size = strlen(buffer);
+               buffer[size] = '\n';
+               buffer[size + 1] = '\0';
+               input.erase();
+
+               inputCorrect++;
+           }
+           else if(input == "LIST"){
+               std::cout << "LIST command " << std::endl;
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveUser("");
+               inputs.push_back(input);
+               input.erase();
+
+               for(auto & iter : inputs){
+                   input += iter;
+                   input += '\n';
+               }
+
+               strcpy(buffer, input.c_str());
+               size = strlen(buffer);
+               buffer[size] = '\n';
+               buffer[size + 1] = '\0';
+               input.erase();
+
+               inputCorrect++;
+           }
+           else if(input == "READ"){
+               std::cout << "READ command " << std::endl;
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveUser("");
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveNumber();
+               inputs.push_back(input);
+               input.erase();
+
+               for(auto & iter : inputs){
+                   input += iter;
+                   input += '\n';
+               }
+
+               strcpy(buffer, input.c_str());
+               size = strlen(buffer);
+               buffer[size] = '\n';
+               buffer[size + 1] = '\0';
+               input.erase();
+
+               inputCorrect++;
+           }
+           else if(input == "DEL"){
+               std::cout << "DEL command " << std::endl;
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveUser("");
+               inputs.push_back(input);
+               input.erase();
+
+               input = receiveNumber();
+               inputs.push_back(input);
+               input.erase();
+
+               for(auto & iter : inputs){
+                   input += iter;
+                   input += '\n';
+               }
+
+               strcpy(buffer, input.c_str());
+               size = strlen(buffer);
+               buffer[size] = '\n';
+               buffer[size + 1] = '\0';
+               input.erase();
+
+               inputCorrect++;
+           }
+           else if(input == "QUIT" || input == "quit"){
+                isQuit++;
+                inputCorrect++;
+           }
+           else{
+               std::cout << "Unknown command. Please enter your commands (SEND/LIST/...)" << std::endl;
+           }
+       }
+       inputCorrect = 0;
 
          //////////////////////////////////////////////////////////////////////
          // SEND DATA
-         // https://man7.org/linux/man-pages/man2/send.2.html
-         // send will fail if connection is closed, but does not set
-         // the error of send, but still the count of bytes sent
-         if ((send(create_socket, buffer, size, 0)) == -1) 
-         {
-            // in case the server is gone offline we will still not enter
-            // this part of code: see docs: https://linux.die.net/man/3/send
-            // >> Successful completion of a call to send() does not guarantee 
-            // >> delivery of the message. A return value of -1 indicates only 
-            // >> locally-detected errors.
-            // ... but
-            // to check the connection before send is sense-less because
-            // after checking the communication can fail (so we would need
-            // to have 1 atomic operation to check...)
+         std::cout << buffer << " sent" << std::endl;
+         if ((send(create_socket, buffer, size, 0)) == -1){
             perror("send error");
             break;
          }
@@ -139,40 +316,28 @@ int main(int argc, char **argv)
          // solution 2: add an infrastructure component for messaging (broker)
          //
          size = recv(create_socket, buffer, BUF - 1, 0);
-         if (size == -1)
-         {
+         if (size == -1){
             perror("recv error");
-            break;
+             break;
          }
-         else if (size == 0)
-         {
+         else if (size == 0){
             printf("Server closed remote socket\n"); // ignore error
-            break;
+             break;
          }
-         else
-         {
+         else{
             buffer[size] = '\0';
             printf("<< %s\n", buffer); // ignore error
-            if (strcmp("OK", buffer) != 0)
-            {
-               fprintf(stderr, "<< Server error occured, abort\n");
-               break;
-            }
          }
-      }
-   } while (!isQuit);
+   } while(!isQuit);
 
    ////////////////////////////////////////////////////////////////////////////
    // CLOSES THE DESCRIPTOR
-   if (create_socket != -1)
-   {
-      if (shutdown(create_socket, SHUT_RDWR) == -1)
-      {
+   if (create_socket != -1){
+      if (shutdown(create_socket, SHUT_RDWR) == -1){
          // invalid in case the server is gone already
          perror("shutdown create_socket"); 
       }
-      if (close(create_socket) == -1)
-      {
+      if (close(create_socket) == -1){
          perror("close create_socket");
       }
       create_socket = -1;
